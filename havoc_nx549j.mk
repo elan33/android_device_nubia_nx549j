37dcb8808f5296c9b9e4070dc28afac61df38528
jsbeyond <67664518@qq.com>
2019-01-30 10:27:45 +0800

    Init Havoc...

--- havoc_nx549j.mk
+++ havoc_nx549j.mk
@@ -24,20 +24,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Havoc stuff.
+$(call inherit-product, vendor/havoc/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/nubia/nx549j/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_nx549j
+PRODUCT_NAME := havoc_nx549j
 PRODUCT_DEVICE := nx549j
 PRODUCT_BRAND := nubia
 PRODUCT_MODEL := Nubia Z11 mini s
 PRODUCT_MANUFACTURER := nubia
 
 PRODUCT_GMS_CLIENTID_BASE := android-nubia
+
+PRODUCT_PROPERTY_OVERRIDES += \
+        ro.havoc.maintainer=板车大叔 
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="NX549J-user 6.0.1 MMB29M eng.nubia.20170919.120125 release-keys" \
